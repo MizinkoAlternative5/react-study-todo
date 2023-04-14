bd2bad4d0661d4024de832359fd192e60757fbf8
MizinkoAlternative5 <67320419+MizinkoAlternative5@users.noreply.github.com>
2023-04-14 14:44:25 +0900

    準備状態

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import "./styles.css";
+import React, { useState } from "react";
 
-export default function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <React.Fragment>
+      <h1>hello</h1>
+    </React.Fragment>
   );
-}
+};
+
+export default App;
